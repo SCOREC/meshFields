72a69e0a00a1059ca643e8fae40574ec8461d2d9
Henry Cullom <henrycullom@gmail.com>
2022-10-05 16:57:55 -0400

    SliceWrapper separated into hpp and cpp

---
+++ src/SliceWrapper.hpp
@@ -0,0 +1,53 @@
+#ifndef slicewrapper_hpp
+#define slicewrapper_hpp
+
+#include <Cabana_Core.hpp>
+
+using member_type = double;
+using MemorySpace = Kokkos::CudaSpace;
+using ExecutionSpace = Kokkos::Cuda;
+using DeviceType = Kokkos::Device<ExecutionSpace, MemorySpace>;
+
+template< class SliceType >
+struct SliceWrapper {
+
+  SliceType st_; //store the underlying instance
+
+  SliceWrapper(SliceType st) : st_(st)  {}
+  
+  KOKKOS_INLINE_FUNCTION
+  member_type& access(const int s, const int a, int i) const {
+    return st_.access(s,a,i);
+  }
+  int arraySize(int s) {
+    return st_.arraySize(s);
+  }
+  int numSoA() {
+    return st_.numSoA();
+  }
+};
+
+using namespace Cabana;
+
+template <class T, int width, int vecLen>
+class CabSliceFactory {
+  using DataTypes = Cabana::MemberTypes<T[width]>;
+  using member_slice_t = 
+    Cabana::Slice<T[width], DeviceType, 
+		  Cabana::DefaultAccessMemory, 
+		  vecLen, width*vecLen>;
+  using wrapper_slice_t = SliceWrapper<member_slice_t>;
+
+  Cabana::AoSoA<DataTypes, DeviceType, vecLen> aosoa; 
+  
+public:
+  wrapper_slice_t makeSliceCab() {
+    auto slice0 = Cabana::slice<0>(aosoa);
+    return wrapper_slice_t(std::move(slice0));
+  }
+  CabSliceFactory(int n) : aosoa("sliceAoSoA", n) {}
+};
+
+
+#endif
+
