997669eb259757ac2ddf3e5c759248365f4ee870
Henry Cullom <henrycullom@gmail.com>
2022-10-13 14:01:53 -0400

    added more tests

--- test/SliceWrapper.cpp
+++ test/SliceWrapper.cpp
@@ -1,11 +1,31 @@
 #include "SliceWrapper.hpp"
+#include <stdio.h>
 
-int main(int argc, char* argv[]) {
-  // AoSoA parameters
-  int num_tuples = 10;
+int single_type_test(int num_tuples) {
+  using ExecutionSpace = Kokkos::DefaultExecutionSpace;
+  using MemorySpace = ExecutionSpace::memory_space;
   
-  Kokkos::ScopeGuard scope_guard(argc, argv);
+  // Slice Wrapper Factory
+  CabSliceFactory<ExecutionSpace, MemorySpace,
+		  double> cabSliceFactory(num_tuples);
+  
+  auto slice_wrapper0 = cabSliceFactory.makeSliceCab<0>();
+  
+  // simd_parallel_for setup
+  Cabana::SimdPolicy<cabSliceFactory.vecLen, ExecutionSpace> simd_policy(0, num_tuples);
 
+  // kernel that reads and writes
+  auto vector_kernel = KOKKOS_LAMBDA(const int s, const int a) {
+    double x = 42/(s+a+1.3);
+    slice_wrapper0.access(s,a) = x;
+    printf("SW0 value: %lf\n", slice_wrapper0.access(s,a));
+  };
+
+  Cabana::simd_parallel_for(simd_policy, vector_kernel, "parallel_for_cabSliceFactory");
+  return 0;
+}
+
+int multi_type_test(int num_tuples) {
   using ExecutionSpace = Kokkos::DefaultExecutionSpace;
   using MemorySpace = ExecutionSpace::memory_space;
   
@@ -36,7 +56,70 @@ int main(int argc, char* argv[]) {
   };
 
   Cabana::simd_parallel_for(simd_policy, vector_kernel, "parallel_for_cabSliceFactory");
+  return 0;
+}
 
+int many_type_test(int num_tuples) {
+  using ExecutionSpace = Kokkos::DefaultExecutionSpace;
+  using MemorySpace = ExecutionSpace::memory_space;
+  
+  // Slice Wrapper Factory
+  CabSliceFactory<ExecutionSpace, MemorySpace,
+		  double, short int, float, char, int,
+		  char, double, long unsigned int,
+		  long double> cabSliceFactory(num_tuples);
+  
+  auto slice_wrapper0 = cabSliceFactory.makeSliceCab<0>();
+  auto slice_wrapper1 = cabSliceFactory.makeSliceCab<1>();
+  auto slice_wrapper2 = cabSliceFactory.makeSliceCab<2>();
+  auto slice_wrapper3 = cabSliceFactory.makeSliceCab<3>();
+  auto slice_wrapper4 = cabSliceFactory.makeSliceCab<4>();
+  auto slice_wrapper5 = cabSliceFactory.makeSliceCab<5>();
+  auto slice_wrapper6 = cabSliceFactory.makeSliceCab<6>();
+  auto slice_wrapper7 = cabSliceFactory.makeSliceCab<7>();
+  auto slice_wrapper8 = cabSliceFactory.makeSliceCab<8>();
+  
+  // simd_parallel_for setup
+  Cabana::SimdPolicy<cabSliceFactory.vecLen, ExecutionSpace> simd_policy(0, num_tuples);
+
+  // kernel that reads and writes
+  auto vector_kernel = KOKKOS_LAMBDA(const int s, const int a) {
+    double x = 42/(s+a+1.3);
+    slice_wrapper0.access(s,a) = x;
+    slice_wrapper1.access(s,a) = s+a;
+    slice_wrapper2.access(s,a) = float(x);
+    slice_wrapper3.access(s,a) = 'a'+s+a;
+    slice_wrapper4.access(s,a) = int(s+a/x);
+    slice_wrapper5.access(s,a) = 'a'+((s*a+a) % 26);
+    slice_wrapper6.access(s,a) = (s+a+a+s*s)*x;
+    slice_wrapper7.access(s,a) = (s+a)*num_tuples/(s+2);
+    slice_wrapper8.access(s,a) = (x+s+a)/(x*x);
+    printf("SW0 value: %lf\n", slice_wrapper0.access(s,a));
+    printf("SW1 value: %d\n", slice_wrapper1.access(s,a));
+    printf("SW2 value: %f\n", slice_wrapper2.access(s,a));
+    printf("SW3 value: %c\n", slice_wrapper3.access(s,a));
+    printf("SW4 value: %d\n", slice_wrapper4.access(s,a));
+    printf("SW5 value: %c\n", slice_wrapper5.access(s,a));
+    printf("SW6 value: %lf\n", slice_wrapper6.access(s,a));
+    printf("SW7 value: %lu\n", slice_wrapper7.access(s,a));
+    printf("SW8 value: %Lf\n", slice_wrapper8.access(s,a));
+  };
+
+  Cabana::simd_parallel_for(simd_policy, vector_kernel, "parallel_for_cabSliceFactory");
+  return 0;
+}
+
+
+int main(int argc, char* argv[]) {
+  // AoSoA parameters
+  int num_tuples = 50;
+  
+  Kokkos::ScopeGuard scope_guard(argc, argv);
+
+  single_type_test(num_tuples);
+  multi_type_test(num_tuples);
+  many_type_test(num_tuples);
+  
   assert(cudaSuccess == cudaDeviceSynchronize());
   printf("done\n");
 
