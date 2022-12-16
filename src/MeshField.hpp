6fd2faab23691b984ef4ffa9bc375bee7340c9e0
Henry Cullom <henrycullom@gmail.com>
2022-12-16 14:12:57 -0500

    added better comments on src files

--- src/MeshField.hpp
+++ src/MeshField.hpp
@@ -18,6 +18,13 @@ public:
 
   Field(Slice s) : slice(s) {}
 
+  /* access functions
+     
+     The access functions are used to get a specific element from a Field. 
+     If the user creates a field of an array, the third argument will be the index of the array.
+     If the user craetes a field of a 2d array, the third and fourth argument will indexes the two arrays, and so on
+  */
+  
   KOKKOS_INLINE_FUNCTION
   auto &operator()(int s, int a) const { return slice.access(s, a); }
 
@@ -40,36 +47,56 @@ template <class Controller> class MeshField {
   Controller sliceController;
 
 public:
-  MeshField(Controller sc) : sliceController(std::move(sc)) {}
 
+  // constructor
+  
+  MeshField(Controller controller) : sliceController(std::move(controller)) {}
+
+  /* makeField
+     
+     makeField makes a field from the underlying controller using the index given by the user
+  */
+  
   template <std::size_t index> auto makeField() {
     auto slice = sliceController.template makeSlice<index>();
     return Field(std::move(slice));
   }
 
+  /* setField
+     
+     fills a field from a Kokkos::View with a parallel_for
+  */
+  
   template <class Field, class View> void setField(Field &field, View &view) {
     auto indexToSA = sliceController.indexToSA;
     Kokkos::parallel_for(
         "FillFieldFromViewLoop", sliceController.size(),
         KOKKOS_LAMBDA(const int &i) {
-          int s;
-          int a;
+          int s,a;
           indexToSA(i, s, a);
           field(s, a) = view(i);
         });
   }
 
+  /* pre-defined reductions
+     
+     These reductions take as input a field and return a single value
+     sum - the sum of all the values in the field
+     min - the minimum value in the field
+     max - the maximum value in the field
+     mean - the average value of the field (will alwyas be a double)
+  */
+  
   template <class FieldType, class T = typename FieldType::Type>
   T sum(FieldType &field) {
     T result;
     auto indexToSA = sliceController.indexToSA;
-    auto reduction_kernel = KOKKOS_LAMBDA(const int &i, T &lsum) {
-      int s;
-      int a;
+    auto reductionKernel = KOKKOS_LAMBDA(const int &i, T &lsum) {
+								  int s,a;
       indexToSA(i, s, a);
       lsum += field(s, a);
     };
-    sliceController.parallel_reduce(reduction_kernel, result, "sum_reduce");
+    sliceController.parallel_reduce(reductionKernel, result, "sum_reduce");
     return result;
   }
 
@@ -77,14 +104,13 @@ public:
   T min(FieldType &field) {
     T min;
     auto indexToSA = sliceController.indexToSA;
-    auto reduce_kernel = KOKKOS_LAMBDA(const int &i, T &lmin) {
-      int s;
-      int a;
+    auto reductionKernel = KOKKOS_LAMBDA(const int &i, T &lmin) {
+							       int s,a;
       indexToSA(i, s, a);
       lmin = lmin < field(s, a) ? lmin : field(s, a);
     };
     auto reducer = Kokkos::Min<T>(min);
-    sliceController.parallel_reduce(reduce_kernel, reducer, "min_reduce");
+    sliceController.parallel_reduce(reductionKernel, reducer, "min_reduce");
     return min;
   }
 
@@ -92,14 +118,13 @@ public:
   T max(FieldType &field) {
     T max;
     auto indexToSA = sliceController.indexToSA;
-    auto reduce_kernel = KOKKOS_LAMBDA(const int &i, T &lmax) {
-      int s;
-      int a;
+    auto reductionKernel = KOKKOS_LAMBDA(const int &i, T &lmax) {
+								 int s,a;
       indexToSA(i, s, a);
       lmax = lmax > field(s, a) ? lmax : field(s, a);
     };
     auto reducer = Kokkos::Max<T>(max);
-    sliceController.parallel_reduce(reduce_kernel, reducer, "max_reduce");
+    sliceController.parallel_reduce(reductionKernel, reducer, "max_reduce");
     return max;
   }
 
@@ -107,22 +132,33 @@ public:
     return static_cast<double>(sum(field)) / sliceController.size();
   }
 
+  /* Parallel functions
+     
+     These functions run the given kernel in parallel on the GPU
+     (unless the execution space is serial)
+     parallel_for - a for loop that will iterate from lowerBound to upperbound
+     parallel_reduce - a way for a user to pass in a reduction kernel and a reducer to make their own reductions
+     parallel_scan - a scan can be used to create a view where each value is the sum of the previous values.
+  */
+  
   template <typename FunctorType>
-  void parallel_for(int lower_bound, int upper_bound,
-                    FunctorType &vector_kernel, std::string tag) {
-    sliceController.parallel_for(lower_bound, upper_bound, vector_kernel, tag);
+  void parallel_for(int lowerBound, int upperBound,
+                    FunctorType &vectorKernel, std::string tag) {
+    sliceController.parallel_for(lowerBound, upperBound, vectorKernel, tag);
   }
 
   template <typename FunctorType, class ReducerType>
-  void parallel_reduce(FunctorType &reduction_kernel, ReducerType &reducer,
-                       std::string tag) {
-    sliceController.parallel_reduce(reduction_kernel, reducer, tag);
+  void parallel_reduce(FunctorType &reductionKernel, ReducerType &reducer, std::string tag) {
+    sliceController.parallel_reduce(reductionKernel, reducer, tag);
   }
 
   template <typename KernelType>
-  void parallel_scan(KernelType &binOp, std::string tag) {
-    Kokkos::parallel_scan(tag, sliceController.size() + 1, binOp);
+  void parallel_scan(KernelType &scanKernel, std::string tag) {
+    Kokkos::parallel_scan(tag, sliceController.size() + 1, scanKernel);
   }
+
 };
+  
 } // namespace MeshField
+
 #endif
--- src/SliceWrapper.hpp
+++ src/SliceWrapper.hpp
@@ -7,7 +7,7 @@ namespace SliceWrapper {
 
 template <class SliceType, class T> struct SliceWrapper {
 
-  SliceType st_; // store the underlying instance
+  SliceType st_;
 
   typedef T Type;
 
@@ -15,14 +15,24 @@ template <class SliceType, class T> struct SliceWrapper {
 
   SliceWrapper() {}
 
+  /* access functions
+     
+     The access functions are used to get a specific element from a Field. 
+     If the user creates a field of an array, the third argument will be the index of the array.
+     If the user craetes a field of a 2d array, the third and fourth argument will indexes the two arrays, and so on
+  */
+  
   KOKKOS_INLINE_FUNCTION
   T &access(int s, int a) const { return st_.access(s, a); }
+  
   KOKKOS_INLINE_FUNCTION
   auto &access(int s, int a, int i) const { return st_.access(s, a, i); }
+
   KOKKOS_INLINE_FUNCTION
   auto &access(int s, int a, int i, int j) const {
     return st_.access(s, a, i, j);
   }
+
   KOKKOS_INLINE_FUNCTION
   auto &access(int s, int a, int i, int j, int k) const {
     return st_.access(s, a, i, j, k);
@@ -33,16 +43,20 @@ using namespace Cabana;
 
 template <class ExecutionSpace, class MemorySpace, class... Ts>
 class CabSliceController {
+
   // type definitions
   using TypeTuple = std::tuple<Ts...>;
   using DeviceType = Kokkos::Device<ExecutionSpace, MemorySpace>;
   using DataTypes = Cabana::MemberTypes<Ts...>;
 
 public:
+  
   static constexpr int vecLen =
       Cabana::AoSoA<DataTypes, DeviceType>::vector_length;
 
 private:
+
+  // all the type defenitions that are needed us to get the type of the slice returned by the underlying AoSoA
   using soa_t = SoA<DataTypes, vecLen>;
 
   template <std::size_t index>
@@ -65,6 +79,7 @@ private:
   const int num_tuples;
 
 public:
+  // a tool that allows a single index to be converted into two indexes that can be used for slice traversal
   struct IndexToSA {
     IndexToSA(int vecLen_in) : vecLen(vecLen_in) {}
     int vecLen;
@@ -77,39 +92,51 @@ public:
 
   IndexToSA indexToSA;
 
+  // constructors (default constructor necessary)
+  
+  CabSliceController() {}
+
+  CabSliceController(int n) : aosoa("sliceAoSoA", n), num_tuples(n), indexToSA(aosoa.vector_length) {}
+
+  // size function to get the number of tuples
+  
   int size() { return num_tuples; }
 
-  template <typename FunctorType, typename ReductionType>
-  void parallel_reduce(FunctorType &reduceKernel, ReductionType &reductionType,
-                       std::string tag) {
-    Kokkos::RangePolicy<ExecutionSpace> policy(0, num_tuples);
-    Kokkos::parallel_reduce(tag, policy, reduceKernel, reductionType);
-  }
-
-  template <typename FunctorType>
-  void parallel_for(int lower_bound, int upper_bound, FunctorType &vectorKernel,
-                    std::string tag) {
-    Cabana::SimdPolicy<vecLen, ExecutionSpace> simd_policy(lower_bound,
-                                                           upper_bound);
-    Cabana::simd_parallel_for(simd_policy, vectorKernel, tag);
-  }
-
+  /* make a slice of the underlying AoSoA based on a user specified index
+    
+     get the type of the value in the slice at the index
+     calculate the 'stride' value, needed for construction of the slice
+     create the slice
+     return the wrapped slice  
+  */
+  
   template <std::size_t index> auto makeSlice() {
     using type = std::tuple_element_t<index, TypeTuple>;
     const int stride = sizeof(soa_t) / sizeof(member_value_t<index>);
     auto slice = Cabana::slice<index>(aosoa);
     return wrapper_slice_t<type, stride>(std::move(slice));
   }
+  
+  /* Parallel functions
+     
+     These functions run the given kernel in parallel on the GPU
+     (unless the execution space is serial)
+     parallel_for - a for loop that will iterate from lowerBound to upperbound
+     parallel_reduce - a way for a user to pass in a reduction kernel and a reducer to make their own reductions
+  */
 
-  CabSliceController() {}
-
-  CabSliceController(int n)
-      : aosoa("sliceAoSoA", n), num_tuples(n), indexToSA(aosoa.vector_length) {
-    if (sizeof...(Ts) == 0) {
-      throw std::invalid_argument(
-          "Must provide at least one member type in template definition");
-    }
+  template <typename FunctorType, typename ReductionType>
+  void parallel_reduce(FunctorType &reductionKernel, ReductionType &reductionType, std::string tag) {
+    Kokkos::RangePolicy<ExecutionSpace> policy(0, num_tuples);
+    Kokkos::parallel_reduce(tag, policy, reductionKernel, reductionType);
   }
+
+  template <typename FunctorType>
+  void parallel_for(int lowerBound, int upperBound, FunctorType &vectorKernel, std::string tag) {
+    Cabana::SimdPolicy<vecLen, ExecutionSpace> simdPolicy(lowerBound, upperBound);
+    Cabana::simd_parallel_for(simdPolicy, vectorKernel, tag);
+  }
+  
 };
 
 } // namespace SliceWrapper
