009610260973d982a4d4509f745e3bac296d7f42
Preston Waters <prestoncwaters@gmail.com>
2023-06-27 12:45:45 -0400

    CTest: Removed unused arg and enabled test

--- test/testMain.cpp
+++ test/testMain.cpp
@@ -176,7 +176,6 @@ void testSetField() {
 }
 
 int main(int argc, char *argv[]) {
-  int num_tuples = (argc < 2) ? (1000) : (atoi(argv[1]));
   Kokkos::ScopeGuard scope_guard(argc, argv);
   testParallelScan();
   testSetField();
