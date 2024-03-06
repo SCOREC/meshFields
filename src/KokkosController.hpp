b676032db9c9b6df7e9fa0e2dcfb13554df36709
Preston Waters <108288614+PrestonH2O@users.noreply.github.com>
2024-03-06 15:17:52 -0500

    Clang formatting fix (#33)
    new checkout action, clang formatting

--- src/KokkosController.hpp
+++ src/KokkosController.hpp
@@ -1,242 +1,237 @@
 #ifndef kokkosslicewrapper_hpp
 #define kokkosslicewrapper_hpp
 
-#include <tuple>
-#include <string>
-#include <vector>
 #include <cassert>
-#include <iterator>
-#include <typeinfo> // typeid
-#include <type_traits>
 #include <initializer_list>
+#include <iterator>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <typeinfo> // typeid
+#include <vector>
 
-#include <Kokkos_Core.hpp>
-#include "MeshField_Utility.hpp"
 #include "MeshField_Macros.hpp"
-
+#include "MeshField_Utility.hpp"
+#include <Kokkos_Core.hpp>
 
 namespace Controller {
 
-template <class SliceType, class T>
-struct KokkosSliceWrapper{
-  
+template <class SliceType, class T> struct KokkosSliceWrapper {
+
   SliceType slice;
   int dimensions[5];
   typedef T Type;
   static const int MAX_RANK = 5;
   static const std::size_t RANK = Kokkos::View<T>::rank;
 
-  KokkosSliceWrapper(SliceType slice_in, int* sizes) : slice(slice_in) {
-    for( int i = 0; i < MAX_RANK; i++ ) dimensions[i] = sizes[i];
+  KokkosSliceWrapper(SliceType slice_in, int *sizes) : slice(slice_in) {
+    for (int i = 0; i < MAX_RANK; i++)
+      dimensions[i] = sizes[i];
   }
   KokkosSliceWrapper() {}
-  
+
   KOKKOS_INLINE_FUNCTION
-  auto size(int i) const { 
-    assert( i >= 0 );
-    assert( i < MAX_RANK );
-    return dimensions[i]; 
+  auto size(int i) const {
+    assert(i >= 0);
+    assert(i < MAX_RANK);
+    return dimensions[i];
   }
 
   /* 1D Access */
   KOKKOS_INLINE_FUNCTION
-  auto &operator()( int s ) const { return slice(s); }
-  
-  KOKKOS_INLINE_FUNCTION
-  auto &operator()( int s, int a ) const { return slice(s,a); }
+  auto &operator()(int s) const { return slice(s); }
 
   KOKKOS_INLINE_FUNCTION
-  auto &operator()( int s, int a, int i ) const { return slice(s,a,i); }
+  auto &operator()(int s, int a) const { return slice(s, a); }
 
   KOKKOS_INLINE_FUNCTION
-  auto &operator()( int s, int a, int i, int j ) 
-    const { return slice(s,a,i,j); }
-  
+  auto &operator()(int s, int a, int i) const { return slice(s, a, i); }
+
   KOKKOS_INLINE_FUNCTION
-  auto &operator()( int s, int a, int i, int j, int k ) 
-    const { return slice(s,a,i,j,k); }
+  auto &operator()(int s, int a, int i, int j) const {
+    return slice(s, a, i, j);
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  auto &operator()(int s, int a, int i, int j, int k) const {
+    return slice(s, a, i, j, k);
+  }
 };
-
 
 template <class MemorySpace, class ExecutionSpace, typename... Ts>
 class KokkosController {
-  
+
   // type definitions
   using TypeTuple = std::tuple<Ts...>;
 
 private:
   // all the type defenitions that are needed us to get the type of the slice
   // returned by the underlying AoSoA
-  template<std::size_t index> 
-  using member_data_t = 
+  template <std::size_t index>
+  using member_data_t =
       typename std::tuple_element<index, std::tuple<Ts...>>::type;
 
   template <std::size_t index>
   using member_value_t =
       typename std::remove_all_extents<member_data_t<index>>::type;
 
-  template <class T>
-  using member_slice_t =
-      Kokkos::View<T,MemorySpace>;
+  template <class T> using member_slice_t = Kokkos::View<T, MemorySpace>;
 
   template <class T>
   using wrapper_slice_t = KokkosSliceWrapper<member_slice_t<T>, T>;
 
-  template<typename... Tx>
-  auto construct( std::vector<int> &dims ) {
+  template <typename... Tx> auto construct(std::vector<int> &dims) {
     return std::tuple<Kokkos::View<Tx>...>{create_view<Tx>("view", dims)...};
   }
-  
-  template<typename Tx>
-  Kokkos::View<Tx,MemorySpace> 
-  create_view( std::string tag, std::vector<int> &dims ) { 
+
+  template <typename Tx>
+  Kokkos::View<Tx, MemorySpace> create_view(std::string tag,
+                                            std::vector<int> &dims) {
     int rank = Kokkos::View<Tx>::rank;
     int dynamic = Kokkos::View<Tx>::rank_dynamic;
-    assert( rank <= MAX_RANK );
-    assert( rank >= 0 );
+    assert(rank <= MAX_RANK);
+    assert(rank >= 0);
 
-    if( rank == 0 ) return Kokkos::View<Tx,MemorySpace>(tag);
-    Kokkos::View<Tx,MemorySpace> rt;
-    switch( dynamic ) {
-      case 1:
-        rt = Kokkos::View<Tx,MemorySpace>(tag, dims[0] );
-        break;
-      case 2:
-        rt = Kokkos::View<Tx,MemorySpace>(tag, dims[0],
-                                               dims[1] );
-        break;
-      case 3:
-        rt = Kokkos::View<Tx,MemorySpace>(tag, dims[0],
-                                               dims[1],
-                                               dims[2]);
-        break;
-      case 4:
-        rt = Kokkos::View<Tx,MemorySpace>(tag, dims[0],
-                                               dims[1],
-                                               dims[2],
-                                               dims[3]);
-        break;
-      case 5:
-        rt = Kokkos::View<Tx,MemorySpace>(tag, dims[0],
-                                               dims[1],
-                                               dims[2],
-                                               dims[3],
-                                               dims[4]);
-        break;
-      default:
-        rt = Kokkos::View<Tx,MemorySpace>(tag);
-        break;
+    if (rank == 0)
+      return Kokkos::View<Tx, MemorySpace>(tag);
+    Kokkos::View<Tx, MemorySpace> rt;
+    switch (dynamic) {
+    case 1:
+      rt = Kokkos::View<Tx, MemorySpace>(tag, dims[0]);
+      break;
+    case 2:
+      rt = Kokkos::View<Tx, MemorySpace>(tag, dims[0], dims[1]);
+      break;
+    case 3:
+      rt = Kokkos::View<Tx, MemorySpace>(tag, dims[0], dims[1], dims[2]);
+      break;
+    case 4:
+      rt = Kokkos::View<Tx, MemorySpace>(tag, dims[0], dims[1], dims[2],
+                                         dims[3]);
+      break;
+    case 5:
+      rt = Kokkos::View<Tx, MemorySpace>(tag, dims[0], dims[1], dims[2],
+                                         dims[3], dims[4]);
+      break;
+    default:
+      rt = Kokkos::View<Tx, MemorySpace>(tag);
+      break;
     }
-    
+
     // Places all of the dyanmic ranks into the extent_sizes
-    for( int i = 0; i < dynamic; i++ ) {
+    for (int i = 0; i < dynamic; i++) {
       this->extent_sizes[theta][i] = dims[i];
     }
-    this->theta+=1;
-    dims.erase( dims.begin(), dims.begin()+dynamic );
+    this->theta += 1;
+    dims.erase(dims.begin(), dims.begin() + dynamic);
     return rt;
   }
-  
-  template< typename T1, typename... Tx>
-  void construct_sizes() {
+
+  template <typename T1, typename... Tx> void construct_sizes() {
     int total_rank = Kokkos::View<T1>::rank;
     int dynamic_rank = Kokkos::View<T1>::rank_dynamic;
     const int static_rank = total_rank - dynamic_rank;
-    assert( total_rank <= MAX_RANK );
-    assert( total_rank >= 0 );
+    assert(total_rank <= MAX_RANK);
+    assert(total_rank >= 0);
 
-    switch( static_rank ) {
-      case 1: 
-        extent_sizes[delta][dynamic_rank] = (int)std::extent<T1,0>::value;
-        break;
-      case 2:
-        extent_sizes[delta][dynamic_rank] = (int)std::extent<T1,0>::value;
-        extent_sizes[delta][dynamic_rank+1] = (int)std::extent<T1,1>::value;
-        break;
-      case 3:
-        extent_sizes[delta][dynamic_rank] = (int)std::extent<T1,0>::value;
-        extent_sizes[delta][dynamic_rank+1] = (int)std::extent<T1,1>::value;
-        extent_sizes[delta][dynamic_rank+2] = (int)std::extent<T1,2>::value;
-        break;
-      case 4:
-        extent_sizes[delta][dynamic_rank] = (int)std::extent<T1,0>::value;
-        extent_sizes[delta][dynamic_rank+1] = (int)std::extent<T1,1>::value;
-        extent_sizes[delta][dynamic_rank+2] = (int)std::extent<T1,2>::value;
-        extent_sizes[delta][dynamic_rank+3] = (int)std::extent<T1,3>::value;
-        break;
-      case 5:
-        extent_sizes[delta][0] = (int)std::extent<T1,0>::value;
-        extent_sizes[delta][1] = (int)std::extent<T1,1>::value;
-        extent_sizes[delta][2] = (int)std::extent<T1,2>::value;
-        extent_sizes[delta][3] = (int)std::extent<T1,3>::value;
-        extent_sizes[delta][4] = (int)std::extent<T1,4>::value;
-        break;
-      case 0:
-        break;
-      default:
-        break;
+    switch (static_rank) {
+    case 1:
+      extent_sizes[delta][dynamic_rank] = (int)std::extent<T1, 0>::value;
+      break;
+    case 2:
+      extent_sizes[delta][dynamic_rank] = (int)std::extent<T1, 0>::value;
+      extent_sizes[delta][dynamic_rank + 1] = (int)std::extent<T1, 1>::value;
+      break;
+    case 3:
+      extent_sizes[delta][dynamic_rank] = (int)std::extent<T1, 0>::value;
+      extent_sizes[delta][dynamic_rank + 1] = (int)std::extent<T1, 1>::value;
+      extent_sizes[delta][dynamic_rank + 2] = (int)std::extent<T1, 2>::value;
+      break;
+    case 4:
+      extent_sizes[delta][dynamic_rank] = (int)std::extent<T1, 0>::value;
+      extent_sizes[delta][dynamic_rank + 1] = (int)std::extent<T1, 1>::value;
+      extent_sizes[delta][dynamic_rank + 2] = (int)std::extent<T1, 2>::value;
+      extent_sizes[delta][dynamic_rank + 3] = (int)std::extent<T1, 3>::value;
+      break;
+    case 5:
+      extent_sizes[delta][0] = (int)std::extent<T1, 0>::value;
+      extent_sizes[delta][1] = (int)std::extent<T1, 1>::value;
+      extent_sizes[delta][2] = (int)std::extent<T1, 2>::value;
+      extent_sizes[delta][3] = (int)std::extent<T1, 3>::value;
+      extent_sizes[delta][4] = (int)std::extent<T1, 4>::value;
+      break;
+    case 0:
+      break;
+    default:
+      break;
     }
-    this->delta+=1;
-    if constexpr ( sizeof...(Tx) >= 1 ) construct_sizes<Tx...>();
+    this->delta += 1;
+    if constexpr (sizeof...(Tx) >= 1)
+      construct_sizes<Tx...>();
   }
 
   // member vaiables
   const int num_types = sizeof...(Ts);
   unsigned short delta = 0;
-  unsigned short theta = 0; 
+  unsigned short theta = 0;
   int extent_sizes[sizeof...(Ts)][5];
-  std::tuple<Kokkos::View<Ts,MemorySpace>...> values_;
+  std::tuple<Kokkos::View<Ts, MemorySpace>...> values_;
 
 public:
-  
   typedef ExecutionSpace exe;
   static const int MAX_RANK = 5;
-  
+
   KokkosController() {
-    if constexpr ( sizeof...(Ts) >= 1 ) { construct_sizes<Ts...>(); }
+    if constexpr (sizeof...(Ts) >= 1) {
+      construct_sizes<Ts...>();
+    }
     std::vector<int> obj;
     values_ = construct<Ts...>(obj);
   }
 
   KokkosController(const std::initializer_list<int> items) {
-    if constexpr ( sizeof...(Ts) >= 1 ) { construct_sizes<Ts...>(); }
+    if constexpr (sizeof...(Ts) >= 1) {
+      construct_sizes<Ts...>();
+    }
     std::vector<int> obj(items);
     values_ = construct<Ts...>(obj);
   }
-  
+
   ~KokkosController() = default;
-  
-  int size( int type_index, int dimension_index ) const { 
+
+  int size(int type_index, int dimension_index) const {
     assert(type_index >= 0);
     assert(type_index < num_types);
     assert(dimension_index >= 0);
-    assert(dimension_index < MAX_RANK );
-    return extent_sizes[type_index][dimension_index]; 
+    assert(dimension_index < MAX_RANK);
+    return extent_sizes[type_index][dimension_index];
   }
-  
-  
-  template<std::size_t index> auto makeSlice() {
+
+  template <std::size_t index> auto makeSlice() {
     using type = std::tuple_element_t<index, TypeTuple>;
     int slice_dims[5];
-    for( int i = 0; i < 5; i++ ) slice_dims[i] = this->extent_sizes[index][i];
-    return wrapper_slice_t<type>(std::get<index>(values_),slice_dims);
+    for (int i = 0; i < 5; i++)
+      slice_dims[i] = this->extent_sizes[index][i];
+    return wrapper_slice_t<type>(std::get<index>(values_), slice_dims);
   }
-  
+
   template <typename FunctorType, class IS, class IE>
-  void parallel_for(const std::initializer_list<IS>& start,
-                    const std::initializer_list<IE>& end,
-                    FunctorType &vectorKernel,
-                    std::string tag ) {
+  void parallel_for(const std::initializer_list<IS> &start,
+                    const std::initializer_list<IE> &end,
+                    FunctorType &vectorKernel, std::string tag) {
     constexpr auto RANK = MeshFieldUtil::function_traits<FunctorType>::arity;
-    assert( RANK >= 1 );
-    Kokkos::Array<int64_t,RANK> a_start = MeshFieldUtil::to_kokkos_array<RANK>( start );
-    Kokkos::Array<int64_t,RANK> a_end = MeshFieldUtil::to_kokkos_array<RANK>( end );
-    if constexpr ( RANK == 1 ) {
+    assert(RANK >= 1);
+    Kokkos::Array<int64_t, RANK> a_start =
+        MeshFieldUtil::to_kokkos_array<RANK>(start);
+    Kokkos::Array<int64_t, RANK> a_end =
+        MeshFieldUtil::to_kokkos_array<RANK>(end);
+    if constexpr (RANK == 1) {
       Kokkos::RangePolicy<ExecutionSpace> p(a_start[0], a_end[0]);
-      Kokkos::parallel_for(tag,p,vectorKernel);
+      Kokkos::parallel_for(tag, p, vectorKernel);
     } else {
-      Kokkos::MDRangePolicy<Kokkos::Rank<RANK>, ExecutionSpace> policy(a_start,a_end);
-      Kokkos::parallel_for( tag, policy, vectorKernel );
+      Kokkos::MDRangePolicy<Kokkos::Rank<RANK>, ExecutionSpace> policy(a_start,
+                                                                       a_end);
+      Kokkos::parallel_for(tag, policy, vectorKernel);
     }
   }
 };
--- test/testCabana.cpp
+++ test/testCabana.cpp
@@ -1,19 +1,18 @@
-#include "MeshField.hpp"
 #include "CabanaController.hpp"
 #include "KokkosController.hpp"
+#include "MeshField.hpp"
 #include "MeshField_Macros.hpp"
 #include "MeshField_Utility.hpp"
 
 #include <Cabana_Core.hpp>
 #include <Kokkos_Core.hpp>
 
-#include <vector>
-#include <iostream>
 #include <initializer_list>
+#include <iostream>
 #include <stdio.h>
+#include <vector>
 
 #define TOLERANCE 1e-10;
-
 
 // helper testing functions
 
@@ -27,18 +26,19 @@ bool doubleCompare(double d1, double d2) {
 using ExecutionSpace = Kokkos::DefaultExecutionSpace;
 using MemorySpace = Kokkos::DefaultExecutionSpace::memory_space;
 
-void testMakeSliceCabana( int num_tuples ) {
+void testMakeSliceCabana(int num_tuples) {
   printf("== START testMakeSliceCabana ==\n");
-  using Ctrl = Controller::CabanaController<ExecutionSpace,MemorySpace,double>;
-  Ctrl c( num_tuples );
+  using Ctrl =
+      Controller::CabanaController<ExecutionSpace, MemorySpace, double>;
+  Ctrl c(num_tuples);
   MeshField::MeshField<Ctrl> cabanaMeshField(c);
 
   auto field0 = cabanaMeshField.makeField<0>();
 
-  auto testKernel = KOKKOS_LAMBDA( const int x ) {
+  auto testKernel = KOKKOS_LAMBDA(const int x) {
     double gamma = (double)x;
     field0(x) = gamma;
-    assert(doubleCompare(field0(x),gamma));
+    assert(doubleCompare(field0(x), gamma));
   };
   Kokkos::parallel_for("testMakeSliceCabana()", num_tuples, testKernel);
 
@@ -47,28 +47,32 @@ void testMakeSliceCabana( int num_tuples ) {
 
 void testParallelReduceCabana() {
   printf("== START testParallelReduceCabana ==\n");
-  using Ctrl = Controller::CabanaController<ExecutionSpace,MemorySpace,int>;
+  using Ctrl = Controller::CabanaController<ExecutionSpace, MemorySpace, int>;
   const int N = 9;
-  Ctrl c( N );
+  Ctrl c(N);
   MeshField::MeshField<Ctrl> cabanaMeshField(c);
-  
+
   {
     double result = 0, verify = 0;
-    auto reduce_kernel = KOKKOS_LAMBDA( const int &i, double& lsum ) {
+    auto reduce_kernel = KOKKOS_LAMBDA(const int &i, double &lsum) {
       lsum += i * 1.0;
     };
-    cabanaMeshField.parallel_reduce("CabanaReduceTest1", {0}, {N}, reduce_kernel, result );
-    for( int i = 0; i < N; i++ ) verify+=i*1.0;
+    cabanaMeshField.parallel_reduce("CabanaReduceTest1", {0}, {N},
+                                    reduce_kernel, result);
+    for (int i = 0; i < N; i++)
+      verify += i * 1.0;
     assert(doubleCompare(verify, result));
   }
   {
     double result = 0, verify = 0;
-    auto reduce_kernel = KOKKOS_LAMBDA( const int &i, const int& j, double& lsum ) {
+    auto reduce_kernel =
+        KOKKOS_LAMBDA(const int &i, const int &j, double &lsum) {
       lsum += i * j;
     };
-    cabanaMeshField.parallel_reduce("CabanaReduceTest2", {0,0}, {N,N}, reduce_kernel, result );
-    for( int i = 0; i < N; i++ ) {
-      for( int j = 0; j < N; j++ ) {
+    cabanaMeshField.parallel_reduce("CabanaReduceTest2", {0, 0}, {N, N},
+                                    reduce_kernel, result);
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
         verify += i * j;
       }
     }
@@ -76,14 +80,16 @@ void testParallelReduceCabana() {
   }
   {
     double result = 0, verify = 0;
-    auto reduce_kernel = KOKKOS_LAMBDA( const int &i, const int& j, const int& k, double& lsum ) {
+    auto reduce_kernel =
+        KOKKOS_LAMBDA(const int &i, const int &j, const int &k, double &lsum) {
       lsum += i * j * k;
     };
-    cabanaMeshField.parallel_reduce("CabanaReduceTest3", {0,0,0}, {N,N,N}, reduce_kernel, result );
-    for( int i = 0; i < N; i++ ) {
-      for( int j = 0; j < N; j++ ) {
-        for( int k = 0; k < N; k++ ) { 
-          verify += i * j * k; 
+    cabanaMeshField.parallel_reduce("CabanaReduceTest3", {0, 0, 0}, {N, N, N},
+                                    reduce_kernel, result);
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        for (int k = 0; k < N; k++) {
+          verify += i * j * k;
         }
       }
     }
@@ -92,15 +98,17 @@ void testParallelReduceCabana() {
   }
   {
     double result = 0, verify = 0;
-    auto reduce_kernel = KOKKOS_LAMBDA( const int &i, const int& j, const int& k, const int& l, double& lsum ) {
+    auto reduce_kernel = KOKKOS_LAMBDA(const int &i, const int &j, const int &k,
+                                       const int &l, double &lsum) {
       lsum += i * j * k * l;
     };
-    cabanaMeshField.parallel_reduce("CabanaReduceTest4", {0,0,0,0}, {N,N,N,N}, reduce_kernel, result );
-    for( int i = 0; i < N; i++ ) {
-      for( int j = 0; j < N; j++ ) {
-        for( int k = 0; k < N; k++ ) { 
-          for( int l = 0; l < N; l++ ) { 
-            verify += i * j * k * l; 
+    cabanaMeshField.parallel_reduce("CabanaReduceTest4", {0, 0, 0, 0},
+                                    {N, N, N, N}, reduce_kernel, result);
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        for (int k = 0; k < N; k++) {
+          for (int l = 0; l < N; l++) {
+            verify += i * j * k * l;
           }
         }
       }
@@ -114,13 +122,18 @@ void testCabanaControllerSize() {
 
   printf("== START testCabanaControllerSize ==\n");
 
-  const int a=6,b=5,c=4,d=3;
-  const int psi[4] = {a,b,c,d};
+  const int a = 6, b = 5, c = 4, d = 3;
+  const int psi[4] = {a, b, c, d};
 
-  using simple = Controller::CabanaController<ExecutionSpace,MemorySpace,int[b]>;
-  using multi = Controller::CabanaController<ExecutionSpace,MemorySpace,int[b][c][d],char[b][c][d], bool[b][c][d]>;
-  using varied = Controller::CabanaController<ExecutionSpace,MemorySpace,double[b][c], int, float[b][c][d], char[b]>;
-  using empty = Controller::CabanaController<ExecutionSpace,MemorySpace, int>;
+  using simple =
+      Controller::CabanaController<ExecutionSpace, MemorySpace, int[b]>;
+  using multi =
+      Controller::CabanaController<ExecutionSpace, MemorySpace, int[b][c][d],
+                                   char[b][c][d], bool[b][c][d]>;
+  using varied =
+      Controller::CabanaController<ExecutionSpace, MemorySpace, double[b][c],
+                                   int, float[b][c][d], char[b]>;
+  using empty = Controller::CabanaController<ExecutionSpace, MemorySpace, int>;
 
   simple c1(a);
   multi c2(a);
@@ -131,26 +144,31 @@ void testCabanaControllerSize() {
   MeshField::MeshField<multi> multi_kok(c2);
   MeshField::MeshField<varied> varied_kok(c3);
   MeshField::MeshField<empty> empty_kok(c4);
-  
+
   // simple_kok
-  for( int i = 0; i < 2; i++ ) { assert( simple_kok.size(0,i) == psi[i] ); }
-  
+  for (int i = 0; i < 2; i++) {
+    assert(simple_kok.size(0, i) == psi[i]);
+  }
+
   // multi_kok
-  for( int i = 0; i < 3; i++ ) {
-    for( int j = 0; j < 4; j++ ) {
-      assert( multi_kok.size(i,j) == psi[j] );
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 4; j++) {
+      assert(multi_kok.size(i, j) == psi[j]);
     }
-  } 
+  }
 
   // varied_kok
-  for( int i = 0; i < 3; i++ ) assert( varied_kok.size(0,i) == psi[i] );
-                               assert( varied_kok.size(1,0) == psi[0] );
-  for( int i = 0; i < 4; i++ ) assert( varied_kok.size(2,i) == psi[i] );
-  for( int i = 0; i < 2; i++ ) assert( varied_kok.size(3,i) == psi[i] );
-  
+  for (int i = 0; i < 3; i++)
+    assert(varied_kok.size(0, i) == psi[i]);
+  assert(varied_kok.size(1, 0) == psi[0]);
+  for (int i = 0; i < 4; i++)
+    assert(varied_kok.size(2, i) == psi[i]);
+  for (int i = 0; i < 2; i++)
+    assert(varied_kok.size(3, i) == psi[i]);
+
   // empty_kok
-  for( int i = 0; i < 4; i++ ) {
-    assert( empty_kok.size(0,i) == 0 );
+  for (int i = 0; i < 4; i++) {
+    assert(empty_kok.size(0, i) == 0);
   }
   printf("== END testCabanaControllerSize ==\n");
 }
@@ -158,13 +176,18 @@ void testCabanaControllerSize() {
 void testCabanaFieldSize() {
   printf("== START testCabanaFieldSize ==\n");
 
-  const int a=6,b=5,c=4,d=3;
-  const int psi[4] = {a,b,c,d};
+  const int a = 6, b = 5, c = 4, d = 3;
+  const int psi[4] = {a, b, c, d};
 
-  using simple = Controller::CabanaController<ExecutionSpace,MemorySpace,int[b]>;
-  using multi = Controller::CabanaController<ExecutionSpace,MemorySpace,int[b][c][d],char[b][c][d], bool[b][c][d]>;
-  using varied = Controller::CabanaController<ExecutionSpace,MemorySpace,double[b][c], int, float[b][c][d], char[b]>;
-  using empty = Controller::CabanaController<ExecutionSpace,MemorySpace, int>;
+  using simple =
+      Controller::CabanaController<ExecutionSpace, MemorySpace, int[b]>;
+  using multi =
+      Controller::CabanaController<ExecutionSpace, MemorySpace, int[b][c][d],
+                                   char[b][c][d], bool[b][c][d]>;
+  using varied =
+      Controller::CabanaController<ExecutionSpace, MemorySpace, double[b][c],
+                                   int, float[b][c][d], char[b]>;
+  using empty = Controller::CabanaController<ExecutionSpace, MemorySpace, int>;
   simple c1(a);
   multi c2(a);
   varied c3(a);
@@ -173,22 +196,23 @@ void testCabanaFieldSize() {
   MeshField::MeshField<multi> multi_kok(c2);
   MeshField::MeshField<varied> varied_kok(c3);
   MeshField::MeshField<empty> empty_kok(c4);
-  
+
   const int MAX_RANK = 4;
 
   { // simple_kok
     auto field0 = simple_kok.makeField<0>();
-    for( int i = 0; i < 2; i++ ) assert( field0.size(i) == psi[i] );
+    for (int i = 0; i < 2; i++)
+      assert(field0.size(i) == psi[i]);
   }
 
   { // multi_kok
     auto field0 = multi_kok.makeField<0>();
     auto field1 = multi_kok.makeField<1>();
     auto field2 = multi_kok.makeField<2>();
-    for( int i = 0; i < MAX_RANK; i++ ) {
-      assert( field0.size(i) == psi[i] );
-      assert( field1.size(i) == psi[i] );
-      assert( field2.size(i) == psi[i] );
+    for (int i = 0; i < MAX_RANK; i++) {
+      assert(field0.size(i) == psi[i]);
+      assert(field1.size(i) == psi[i]);
+      assert(field2.size(i) == psi[i]);
     }
   }
 
@@ -198,24 +222,35 @@ void testCabanaFieldSize() {
     auto field2 = varied_kok.makeField<2>();
     auto field3 = varied_kok.makeField<3>();
 
-    for( int i = 0; i < 3; i++ ) { assert( field0.size(i) == psi[i] ); }
-    for( int i = 0; i < 1; i++ ) { assert( field1.size(i) == psi[i] ); }
-    for( int i = 0; i < 4; i++ ) { assert( field2.size(i) == psi[i] ); }
-    for( int i = 0; i < 2; i++ ) { assert( field3.size(i) == psi[i] ); }
+    for (int i = 0; i < 3; i++) {
+      assert(field0.size(i) == psi[i]);
+    }
+    for (int i = 0; i < 1; i++) {
+      assert(field1.size(i) == psi[i]);
+    }
+    for (int i = 0; i < 4; i++) {
+      assert(field2.size(i) == psi[i]);
+    }
+    for (int i = 0; i < 2; i++) {
+      assert(field3.size(i) == psi[i]);
+    }
   }
   { // empty_kok
     auto field0 = empty_kok.makeField<0>();
-    for( int i = 0; i < MAX_RANK; i++ ) { assert( field0.size(i) == 0 ); }
+    for (int i = 0; i < MAX_RANK; i++) {
+      assert(field0.size(i) == 0);
+    }
   }
   printf("== END testCabanaFieldSize ==\n");
 }
 
 void testCabanaParallelFor() {
   printf("== START testCabanaParallelFor() ==\n");
-  const int x=10,y=9,z=8,a=7;
-  { 
-    using simd_ctrlr = Controller::CabanaController<ExecutionSpace,MemorySpace,int,int[y],
-          int[y][z], int[y][z][a]>;
+  const int x = 10, y = 9, z = 8, a = 7;
+  {
+    using simd_ctrlr =
+        Controller::CabanaController<ExecutionSpace, MemorySpace, int, int[y],
+                                     int[y][z], int[y][z][a]>;
     simd_ctrlr c1(x);
     MeshField::MeshField<simd_ctrlr> mf(c1);
     auto field0 = mf.makeField<0>();
@@ -223,32 +258,31 @@ void testCabanaParallelFor() {
     auto field2 = mf.makeField<2>();
     auto field3 = mf.makeField<3>();
 
-    auto vectorKernel = KOKKOS_LAMBDA( const int& i ) {
+    auto vectorKernel = KOKKOS_LAMBDA(const int &i) {
       field0(i) = i;
       assert(field0(i) == i);
     };
-    mf.parallel_for( {0},{x}, vectorKernel, "simple_loop");
-    
-    auto vectorKernel2 = KOKKOS_LAMBDA( const int& i, const int& j ) {
-      field1(i,j) = i+j;
-      assert(field1(i,j) == i+j);
-    };
-    mf.parallel_for( {0,0},{x,y}, vectorKernel2, "simple_loop");
+    mf.parallel_for({0}, {x}, vectorKernel, "simple_loop");
 
-    auto vectorKernel3 = KOKKOS_LAMBDA( const int& i, const int& j, const int& k ) {
-      field2(i,j,k) = i+j+k;
-      assert( field2(i,j,k) == i+j+k );
+    auto vectorKernel2 = KOKKOS_LAMBDA(const int &i, const int &j) {
+      field1(i, j) = i + j;
+      assert(field1(i, j) == i + j);
     };
-    mf.parallel_for( {0,0,0},{x,y,z}, vectorKernel3, "simple_loop");
+    mf.parallel_for({0, 0}, {x, y}, vectorKernel2, "simple_loop");
 
-
-    auto vectorKernel4 = KOKKOS_LAMBDA( const int& i, const int& j, const int& k,
-                                        const int& l) {
-      field3(i,j,k,l) = i+j+k+l;
-      assert( field3(i,j,k,l) == i+j+k+l );
+    auto vectorKernel3 =
+        KOKKOS_LAMBDA(const int &i, const int &j, const int &k) {
+      field2(i, j, k) = i + j + k;
+      assert(field2(i, j, k) == i + j + k);
     };
-    mf.parallel_for( {0,0,0,0},{x,y,z,a}, vectorKernel4, "simple_loop");
-    
+    mf.parallel_for({0, 0, 0}, {x, y, z}, vectorKernel3, "simple_loop");
+
+    auto vectorKernel4 =
+        KOKKOS_LAMBDA(const int &i, const int &j, const int &k, const int &l) {
+      field3(i, j, k, l) = i + j + k + l;
+      assert(field3(i, j, k, l) == i + j + k + l);
+    };
+    mf.parallel_for({0, 0, 0, 0}, {x, y, z, a}, vectorKernel4, "simple_loop");
   }
 
   printf("== END testCabanaParallelFor() ==\n");
