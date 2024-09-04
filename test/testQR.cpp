397a1dadbb3ed7d64705876bf6a16fb7b03a6ba1
Cameron Smith <smithc11@rpi.edu>
2024-09-04 15:53:02 -0400

    compiles

--- test/testQR.cpp
+++ test/testQR.cpp
@@ -1,8 +1,13 @@
 #include <Kokkos_Core.hpp>
 #include <Kokkos_Random.hpp> //Kokkos::fill_random, Kokkos_Random_XorShift64_Pool
 #include <KokkosBatched_Util.hpp> //KokkosBlas::Algo, KokkosBatched::Diag
-#include <array>
-#include <cstdio>
+#include <KokkosBlas1_set.hpp> //KokkosBlas::TeamVectorSet
+#include <KokkosBlas2_team_gemv.hpp> //KokkosBlas::TeamVectorGemv
+#include <KokkosBatched_Vector.hpp> //KokkosBlas::TeamVectorCopy
+#include <KokkosBatched_QR_Decl.hpp> //KokkosBlas::TeamVectorQR
+#include <KokkosBatched_Copy_Decl.hpp> //KokkosBlas::TeamVectorCopy
+#include <KokkosBatched_Trsv_Decl.hpp> //KokkosBlas::TeamVectorTrsv
+#include <KokkosBatched_ApplyQ_Decl.hpp> //KokkosBlas::TeamVectorApplyQ
 
 template <typename DeviceType, typename MatrixViewType, typename VectorViewType, typename WorkViewType,
           typename AlgoTagType>
@@ -37,39 +42,35 @@ struct Functor_TestBatchedTeamVectorQR {
     member.team_barrier();
 
     /// bb = AA*xx
-    KokkosBlas::TeamVectorGemv<MemberType, KokkosBlas::Trans::NoTranspose, Algo::Gemv::Unblocked>::invoke(member, one, aa, xx, zero,
+    KokkosBlas::TeamVectorGemv<MemberType, KokkosBlas::Trans::NoTranspose, KokkosBlas::Algo::Gemv::Unblocked>::invoke(member, one, aa, xx, zero,
                                                                                               bb);
     member.team_barrier();
 
     /// AA = QR
-    TeamVectorQR<MemberType, AlgoTagType>::invoke(member, aa, tt, ww);
+    KokkosBatched::TeamVectorQR<MemberType, AlgoTagType>::invoke(member, aa, tt, ww);
     member.team_barrier();
 
     /// xx = bb;
-    TeamVectorCopy<MemberType, KokkosBlas::Trans::NoTranspose, 1>::invoke(member, bb, xx);
+    KokkosBatched::TeamVectorCopy<MemberType, KokkosBlas::Trans::NoTranspose, 1>::invoke(member, bb, xx);
     member.team_barrier();
 
     /// xx = Q^{T}xx;
-    TeamVectorApplyQ<MemberType, Side::Left, KokkosBlas::Trans::Transpose, KokkosBlas::Algo::ApplyQ::Unblocked>::invoke(member, aa, tt, xx, ww);
+    KokkosBatched::TeamVectorApplyQ<MemberType, KokkosBatched::Side::Left, KokkosBlas::Trans::Transpose, KokkosBlas::Algo::ApplyQ::Unblocked>::invoke(member, aa, tt, xx, ww);
     member.team_barrier();
 
     /// xx = R^{-1} xx
-    TeamVectorTrsv<MemberType, Uplo::Upper, KokkosBlas::Trans::NoTranspose, KokkosBatched::Diag::NonUnit, KokkosBlas::Algo::Trsv::Unblocked>::invoke(
+    KokkosBatched::TeamVectorTrsv<MemberType, KokkosBatched::Uplo::Upper, KokkosBlas::Trans::NoTranspose, KokkosBatched::Diag::NonUnit, KokkosBlas::Algo::Trsv::Unblocked>::invoke(
         member, one, aa, xx);
   }
 
   inline void run() {
     typedef typename MatrixViewType::non_const_value_type value_type;
-    std::string name_region("KokkosBatched::Test::TeamVectorQR");
-    const std::string name_value_type = Test::value_type_name<value_type>();
-    std::string name                  = name_region + name_value_type;
-    Kokkos::Profiling::pushRegion(name.c_str());
+    std::string name_region("KokkosBatched::TeamVectorQR");
 
     const int league_size = _a.extent(0);
     Kokkos::TeamPolicy<execution_space> policy(league_size, Kokkos::AUTO);
 
-    Kokkos::parallel_for(name.c_str(), policy, *this);
-    Kokkos::Profiling::popRegion();
+    Kokkos::parallel_for(name_region.c_str(), policy, *this);
   }
 };
 
@@ -95,27 +96,26 @@ void impl_test_batched_qr(const int N, const int BlkSize) {
 
   Kokkos::fence();
 
-  Functor_TestBatchedTeamVectorQR<DeviceType, MatrixViewType, VectorViewType, WorkViewType, AlgoTagType>(a, x, b, t, w)
-//      .run();
-//
-//  Kokkos::fence();
-//
-//  /// for comparison send it to host
-//  typename VectorViewType::HostMirror x_host = Kokkos::create_mirror_view(x);
-//  Kokkos::deep_copy(x_host, x);
-//
-//  /// check x = 1; this eps is about 1e-14
-//  typedef typename ats::mag_type mag_type;
-//  const mag_type eps = 1e3 * ats::epsilon();
-//
-//  for (int k = 0; k < N; ++k) {
-//    for (int i = 0; i < BlkSize; ++i) {
-//      const mag_type sum  = ats::abs(x_host(k, i));
-//      const mag_type diff = ats::abs(x_host(k, i) - one);
-//      EXPECT_NEAR_KK(diff / sum, mag_type(0), eps);
-//      // printf("k = %d, i = %d, sum %e diff %e \n", k, i, sum, diff );
-//    }
-//  }
+  Functor_TestBatchedTeamVectorQR<DeviceType, MatrixViewType, VectorViewType, WorkViewType, AlgoTagType>(a, x, b, t, w).run();
+
+  Kokkos::fence();
+
+  /// for comparison send it to host
+  typename VectorViewType::HostMirror x_host = Kokkos::create_mirror_view(x);
+  Kokkos::deep_copy(x_host, x);
+
+  /// check x = 1; this eps is about 1e-14
+  typedef typename ats::mag_type mag_type;
+  const mag_type eps = 1e3 * ats::epsilon();
+
+  for (int k = 0; k < N; ++k) {
+    for (int i = 0; i < BlkSize; ++i) {
+      const mag_type sum  = ats::abs(x_host(k, i));
+      const mag_type diff = ats::abs(x_host(k, i) - one);
+      assert(std::abs(diff / sum - mag_type(0)) < eps);
+      // printf("k = %d, i = %d, sum %e diff %e \n", k, i, sum, diff );
+    }
+  }
 }
 
 using ExecutionSpace = Kokkos::DefaultExecutionSpace;
