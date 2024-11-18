efbc9dd97478ea58bdeee9dc036d3c2ae4e9df7f
Cameron Smith <smithc11@rpi.edu>
2024-11-18 13:34:09 -0500

    doc string

--- src/MeshField_Fail.hpp
+++ src/MeshField_Fail.hpp
@@ -9,7 +9,13 @@ struct exception : public std::runtime_error {
   exception(std::string const &msg_in) : std::runtime_error(msg_in) {}
 };
 
-// print a message then throw an execution
+/** print a message then throw an execution or abort
+ * \brief an exception is thrown if MeshFields_USE_EXCEPTIONS is true,
+ * otherwise abort
+ * \param format (in) printf like format string
+ * \param ... (in) variadic list of args to match the format specifiers used in
+ * the format string
+ */
 void fail(char const *format, ...);
 } // namespace MeshField
 #endif // MESHFIELDS_FAIL_H
