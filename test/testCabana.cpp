cf19c01bbaf9dd3bff54c90662e93f1a84adcf91
Cameron Smith <smithc11@rpi.edu>
2024-12-05 11:52:37 -0500

    cabana is optional

--- test/testCabana.cpp
+++ test/testCabana.cpp
@@ -23,6 +23,10 @@ bool doubleCompare(double d1, double d2) {
   return diff < TOLERANCE;
 }
 
+// returns (x-1)*(x/2) = 1 + 2 + 3 + 4 + ... + x
+KOKKOS_INLINE_FUNCTION
+int seriesSum(int x) { return (int)(((double)x - 1.0)) * (((double)x / 2.0)); }
+
 using ExecutionSpace = Kokkos::DefaultExecutionSpace;
 using MemorySpace = Kokkos::DefaultExecutionSpace::memory_space;
 
@@ -288,6 +292,79 @@ void testCabanaParallelFor() {
   printf("== END testCabanaParallelFor() ==\n");
 }
 
+void testParallelScan() {
+  const int N = 100;
+  printf("== START testParallelScan ==\n");
+  using s_cab =
+      Controller::CabanaController<ExecutionSpace, MemorySpace, int, int>;
+  s_cab c1(N);
+  MeshField::MeshField<s_cab> mfc(c1);
+  auto pre = mfc.makeField<0>();
+  auto post = mfc.makeField<1>();
+
+  auto scan_kernel =
+      KOKKOS_LAMBDA(int i, int &partial_sum, const bool is_final) {
+    if (is_final)
+      pre(i) = partial_sum;
+    partial_sum += i;
+    if (is_final)
+      post(i) = partial_sum;
+  };
+
+  for (int i = 1; i <= N; i++) {
+    int result;
+    mfc.parallel_scan("default", 0, i, scan_kernel, result);
+    assert(result == seriesSum(i));
+  }
+  printf("== END testParallelScan ==\n");
+}
+
+void testSetField() {
+  printf("== START testSetField ==\n");
+  const int N = 10;
+  using cab1 = Controller::CabanaController<ExecutionSpace, MemorySpace, int,
+                                            int[N], int[N][N], int[N][N][N]>;
+  cab1 c1(N);
+  MeshField::MeshField<cab1> mf(c1);
+  auto f1 = mf.makeField<0>();
+  auto f2 = mf.makeField<1>();
+  auto f3 = mf.makeField<2>();
+  auto f4 = mf.makeField<3>();
+
+  Kokkos::View<int *> v1("1", N);
+  Kokkos::View<int **> v2("2", N, N);
+  Kokkos::View<int ***> v3("3", N, N, N);
+  Kokkos::View<int ****> v4("4", N, N, N, N);
+
+  Kokkos::Array start = MeshFieldUtil::to_kokkos_array<4>({0, 0, 0, 0});
+  Kokkos::Array end = MeshFieldUtil::to_kokkos_array<4>({N, N, N, N});
+  Kokkos::MDRangePolicy<Kokkos::Rank<4>> p(start, end);
+
+  Kokkos::parallel_for(
+      "", p,
+      KOKKOS_LAMBDA(const int &i, const int &j, const int &k, const int &l) {
+        v1(i) += i;
+        v2(i, j) += i + j;
+        v3(i, j, k) += i + j + k;
+        v4(i, j, k, l) += i + j + k + l;
+      });
+
+  mf.setField(f1, v1);
+  mf.setField(f2, v2);
+  mf.setField(f3, v3);
+  mf.setField(f4, v4);
+
+  Kokkos::parallel_for(
+      "", p,
+      KOKKOS_LAMBDA(const int &i, const int &j, const int &k, const int &l) {
+        assert(f1(i) == v1(i));
+        assert(f2(i, j) == v2(i, j));
+        assert(f3(i, j, k) == v3(i, j, k));
+        assert(f4(i, j, k, l) == v4(i, j, k, l));
+      });
+  printf("== END testSetField ==\n");
+}
+
 int main(int argc, char *argv[]) {
   int num_tuples = (argc < 2) ? (1000) : (atoi(argv[1]));
   Kokkos::ScopeGuard scope_guard(argc, argv);
@@ -296,5 +373,7 @@ int main(int argc, char *argv[]) {
   testCabanaControllerSize();
   testCabanaFieldSize();
   testCabanaParallelFor();
+  testSetField();
+  testParallelScan();
   return 0;
 }
