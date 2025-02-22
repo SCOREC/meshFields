fa25096511aa4d43a8ffa899982e91ff45966393
Cameron Smith <smithc11@rpi.edu>
2025-02-21 20:54:22 -0500

    run both 2d tri tests

--- test/testElementJacobian2d.cpp
+++ test/testElementJacobian2d.cpp
@@ -36,15 +36,31 @@ struct LinearTriangleToVertexField {
   }
 };
 
+class TriangleTestCase {
+  public:
+    TriangleTestCase(std::vector<MeshField::Real> coords_in,
+                     std::vector<MeshField::Real> jacobian_in,
+                     MeshField::Real jacobianDeterminant_in) :
+      coords(coords_in),
+      jacobian(jacobian_in),
+      jacobianDeterminant(jacobianDeterminant_in)
+    {
+        assert(coords.size() == 6);
+        assert(jacobian.size() == 4);
+    };
+    const std::vector<MeshField::Real> coords;
+    const std::vector<MeshField::Real> jacobian;
+    const MeshField::Real jacobianDeterminant;
+  private:
+    TriangleTestCase();
+};
+
+
 template <typename ShapeField>
-void setVtxCoords(size_t numVerts, size_t meshDim, ShapeField field) {
+void setVtxCoords(size_t numVerts, size_t meshDim, TriangleTestCase testTri, ShapeField field) {
   Kokkos::View<MeshField::Real*, Kokkos::HostSpace> coords_h("coords_h", numVerts*meshDim);
-//  coords_h[0] = 0; coords_h[1] = 0;
-//  coords_h[2] = 1; coords_h[3] = 0;
-//  coords_h[4] = 0; coords_h[5] = 1;
-  coords_h[0] = 0; coords_h[1] = 0;
-  coords_h[2] = 5; coords_h[3] = 1;
-  coords_h[4] = 3; coords_h[5] = 4;
+  for(size_t i=0; i<numVerts*meshDim; i++)
+    coords_h[i] = testTri.coords.at(i);
   auto coords = Kokkos::create_mirror_view_and_copy(ExecutionSpace(), coords_h);
   auto setCoordField = KOKKOS_LAMBDA(const int &vtx) {
     field(0, 0, vtx, MeshField::Vertex) = coords(vtx * meshDim);
@@ -62,34 +78,39 @@ void triJacobian() {
   meshInfo.numEdge = 3;
   meshInfo.numTri = 1;
   auto coordField = MeshField::CreateCoordinateField<ExecutionSpace, MeshField::KokkosController>(meshInfo);
-  setVtxCoords(meshInfo.numVtx, meshInfo.dim, coordField);
 
-  MeshField::FieldElement f(meshInfo.numTri, coordField,
-                            MeshField::LinearTriangleShape(),
-                            LinearTriangleToVertexField());
+  TriangleTestCase rightTriangle({0,0,1,0,0,1},{1,0,0,1},1);
+  TriangleTestCase skewedTriangle({0,0,5,1,3,4},{5,1,3,4},17);
+  for( auto testCase : {rightTriangle,skewedTriangle} ) {
+    setVtxCoords(meshInfo.numVtx, meshInfo.dim, testCase, coordField);
 
-  Kokkos::View<MeshField::Real*[3]> lc("localCoords",1);
-  Kokkos::deep_copy(lc, 1.0 / 2);
-  const auto numPtsPerElement = 1;
-  const auto J = MeshField::getJacobians(f, lc, numPtsPerElement);
-  const auto J_h = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(),J);
-  assert(J_h.rank() == 3);
-  assert(J_h.extent(0) == 1);
-  assert(J_h.extent(1) == 2);
-  assert(J_h.extent(2) == 2);
-  std::cout << "tri jacobian\n" 
-            << J_h(0,0,0) << " " << J_h(0,0,1) << "\n"
-            << J_h(0,1,0) << " " << J_h(0,1,1) << "\n";
-  const auto determinants = MeshField::getJacobianDeterminants(f, J);
-  const auto determinants_h = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(),determinants);
-  std::cout << "tri jacobian determinant " << determinants_h(0) << "\n";
-  assert(std::fabs(J_h(0,0,0) - 1.0) <= MeshField::MachinePrecision);
-  assert(std::fabs(J_h(0,0,1) - 0.0) <= MeshField::MachinePrecision);
-  assert(std::fabs(J_h(0,1,0) - 0.0) <= MeshField::MachinePrecision);
-  assert(std::fabs(J_h(0,1,1) - 1.0) <= MeshField::MachinePrecision);
-  assert(determinants.rank() == 1);
-  assert(determinants.extent(0) == 1);
-  assert(std::fabs(determinants_h(0) - 1.0) <= MeshField::MachinePrecision);
+    MeshField::FieldElement f(meshInfo.numTri, coordField,
+                              MeshField::LinearTriangleShape(),
+                              LinearTriangleToVertexField());
+
+    Kokkos::View<MeshField::Real*[3]> lc("localCoords",1);
+    Kokkos::deep_copy(lc, 1.0 / 2);
+    const auto numPtsPerElement = 1;
+    const auto J = MeshField::getJacobians(f, lc, numPtsPerElement);
+    const auto J_h = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(),J);
+    assert(J_h.rank() == 3);
+    assert(J_h.extent(0) == 1);
+    assert(J_h.extent(1) == 2);
+    assert(J_h.extent(2) == 2);
+    std::cout << "tri jacobian\n"
+              << J_h(0,0,0) << " " << J_h(0,0,1) << "\n"
+              << J_h(0,1,0) << " " << J_h(0,1,1) << "\n";
+    const auto determinants = MeshField::getJacobianDeterminants(f, J);
+    const auto determinants_h = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(),determinants);
+    std::cout << "tri jacobian determinant " << determinants_h(0) << "\n";
+    assert(std::fabs(J_h(0,0,0) - testCase.jacobian.at(0)) <= MeshField::MachinePrecision);
+    assert(std::fabs(J_h(0,0,1) - testCase.jacobian.at(1)) <= MeshField::MachinePrecision);
+    assert(std::fabs(J_h(0,1,0) - testCase.jacobian.at(2)) <= MeshField::MachinePrecision);
+    assert(std::fabs(J_h(0,1,1) - testCase.jacobian.at(3)) <= MeshField::MachinePrecision);
+    assert(determinants.rank() == 1);
+    assert(determinants.extent(0) == 1);
+    assert(std::fabs(determinants_h(0) - testCase.jacobianDeterminant) <= MeshField::MachinePrecision);
+  }
 }
 
 int main(int argc, char **argv) {
