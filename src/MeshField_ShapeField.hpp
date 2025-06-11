4579d520954f5c06a20306aa7f971af6076a8189
Cameron Smith <smithc11@rpi.edu>
2025-06-11 16:25:08 -0400

    wip

--- src/MeshField_ShapeField.hpp
+++ src/MeshField_ShapeField.hpp
@@ -276,7 +276,7 @@ auto CreateCoordinateField(const MeshInfo &meshInfo) {
   using DataType = Real;
   using MemorySpace = typename ExecutionSpace::memory_space;
   const int numComp = meshInfo.dim;
-// FIXME Oversized cabana datatypes when numComp = 1|2
+// FIXME Oversized cabana datatypes when numComp = 1|2, specialize controller template for dimension? //HERE
 #ifdef MESHFIELDS_ENABLE_CABANA
   using Ctrlr = std::conditional_t<
       std::is_same_v<
