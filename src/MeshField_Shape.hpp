01a16db62cf9c6f893dc191d9ed3c9fa806ca091
jvmespark <jamesdpark1@gmail.com>
2025-10-24 13:12:05 -0400

    init

--- src/MeshField_Shape.hpp
+++ src/MeshField_Shape.hpp
@@ -217,5 +217,89 @@ struct QuadraticTetrahedronShape {
   }
 };
 
+struct ReducedQuinticImplicitShape {
+  static const size_t numNodes = 18;
+  static const size_t meshEntDim = 2;
+  constexpr static Mesh_Topology DofHolders[1] = {Vertex};
+  constexpr static size_t Order = 5;
+
+  KOKKOS_INLINE_FUNCTION
+  Kokkos::Array<Real, numNodes> getValues(Vector3 const &xi) const {
+    assert(greaterThanOrEqualZero(xi));
+    assert(sumsToOne(xi));
+
+    const Real L1 = 1.0 - xi[0] - xi[1];
+    const Real L2 = xi[0];
+    const Real L3 = xi[1];
+
+    const Real L1_2 = L1 * L1;
+    const Real L2_2 = L2 * L2;
+    const Real L3_2 = L3 * L3;
+    const Real L1_3 = L1_2 * L1;
+    const Real L2_3 = L2_2 * L2;
+    const Real L3_3 = L3_2 * L3;
+    const Real L1_4 = L1_3 * L1;
+    const Real L2_4 = L2_3 * L2;
+    const Real L3_4 = L3_3 * L3;
+    const Real L1_5 = L1_4 * L1;
+    const Real L2_5 = L2_4 * L2;
+    const Real L3_5 = L3_4 * L3;
+
+    Kokkos::Array<Real, numNodes> N;
+
+    N[0] = L1 * (2 * L1 - 1) * (3 * L1 - 1) * (4 * L1 - 1) * (5 * L1 - 1);
+    N[1] = L2 * (2 * L2 - 1) * (3 * L2 - 1) * (4 * L2 - 1) * (5 * L2 - 1);
+    N[2] = L3 * (2 * L3 - 1) * (3 * L3 - 1) * (4 * L3 - 1) * (5 * L3 - 1);
+    
+    N[3] = 5.0 * L1_4 * L2;
+    N[4] = 5.0 * L2_4 * L1;
+    N[5] = 5.0 * L3_4 * L1;
+
+    N[6] = 5.0 * L1_4 * L3;
+    N[7] = 5.0 * L2_4 * L3;
+    N[8] = 5.0 * L3_4 * L2;
+
+    N[9]  = 10.0 * L1_3 * L2_2;
+    N[10] = 10.0 * L2_3 * L1_2;
+    N[11] = 10.0 * L3_3 * L1_2;
+
+    N[12] = 10.0 * L1_3 * L3_2;
+    N[13] = 10.0 * L2_3 * L3_2;
+    N[14] = 10.0 * L3_3 * L2_2;
+
+    N[15] = 20.0 * L1_2 * L2 * L3;
+    N[16] = 20.0 * L2_2 * L1 * L3;
+    N[17] = 20.0 * L3_2 * L1 * L2;
+
+    return N;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  Kokkos::Array<Vector2, numNodes> getLocalGradients(Vector3 const &xi) const {
+    assert(greaterThanOrEqualZero(xi));
+    assert(sumsToOne(xi));
+
+    const Real L1 = 1.0 - xi[0] - xi[1];
+    const Real L2 = xi[0];
+    const Real L3 = xi[1];
+
+    const Real eps = 1e-6;
+
+    Vector3 xp = {xi[0] + eps, xi[1], 0.0};
+    Vector3 yp = {xi[0], xi[1] + eps, 0.0};
+
+    auto N0 = getValues(xi);
+    auto Nx = getValues(xp);
+    auto Ny = getValues(yp);
+
+    Kokkos::Array<Vector2, numNodes> dN;
+    for (int i = 0; i < numNodes; i++) {
+      dN[i][0] = (Nx[i] - N0[i]) / eps;
+      dN[i][1] = (Ny[i] - N0[i]) / eps;
+    }
+    return dN;
+  }
+};
+
 } // namespace MeshField
 #endif
