fdd8a314974520f627a191c5c7b95692f6efbfe7
Joshua Kloepfer <joshua.kloepfer@gmail.com>
2025-12-24 13:46:28 -0500

    working on mfem test

--- mfemTest/IntegratorMfemTest.cpp
+++ mfemTest/IntegratorMfemTest.cpp
@@ -6,32 +6,33 @@ using namespace std;
 using namespace mfem;
 
 double intFunc(const Vector &x) {
-  return x[0]; 
+  return x[0];
 }
 
-int main(int argc, char *argv[])
-{
-   // 1. Parse command line options.
-   string mesh_file = "../data/star.mesh";
-   int order = 1;
-
-   OptionsParser args(argc, argv);
-   args.AddOption(&mesh_file, "-m", "--mesh", "Mesh file to use.");
-   args.AddOption(&order, "-o", "--order", "Finite element polynomial degree");
-   args.ParseCheck();
-   Mesh mesh(mesh_file);
-
-   H1_FECollection fec(order, mesh.Dimension());
-   FiniteElementSpace fespace(&mesh, &fec);
+int main(int argc, char *argv[]) {
   
-   FunctionCoefficient custom(intFunc);
+  OptionsParser args(argc, argv);
+  int order = 1;
+  int size = 1;
+  int numberOfElements = 1;
+  int type = 1;
+  args.AddOption(&order, "-o", "--order", "Finite element polynomial degree");
+  args.AddOption(&size, "-s", "--size", "Size of mesh");
+  args.AddOption(&numberOfElements, "-n", "--numElem", "number of elements in each direction");
+  args.AddOption(&type, "-t", "--type", "type of element(1 for tet, 0 for triangle");
+  args.ParseCheck();
+  
+  Mesh mesh = Mesh::MakeCartesian3D(numberOfElements, numberOfElements, type == 1 ? numberOfElements : 1, type == 1 ? Element::TETRAHEDRON : Element::TRIANGLE, size, size, type == 1 ? size : 1, false);
+  H1_FECollection fec(order, mesh.Dimension());
+  FiniteElementSpace fespace(&mesh, &fec);
+  
+  FunctionCoefficient custom(intFunc);
 
-   LinearForm b(&fespace);
+  LinearForm b(&fespace);
 
-   b.AddDomainIntegrator(new DomainLFIntegrator(custom)); 
+  b.AddDomainIntegrator(new DomainLFIntegrator(custom)); 
 
-   b.Assemble(); 
-
-   cout << b.Sum() << endl;
-   return 0;
+  b.Assemble(); 
+  cout << b.Sum() << endl;
+  return 0;
 }
--- mfemTest/README.md
+++ mfemTest/README.md
@@ -1,5 +1,5 @@
 Run using g++ IntegratorMfemTest.cpp  -I<path-to-mfem-build> -L<path-to-mfem-build>  -l mfem
-in order to run, use -m to specify the mesh file and -o to specify order
+in order to run, use -m to specify the mesh file, -o to specify order, -s to specify size, -n to specify number of elements in each direction, and -t to specify type of element with 1 for tetrahedron and 0 for triangle.
 mfem version: https://github.com/mfem/mfem/tree/2caa75e35a54df93d19f23655170254556dfc081
 For the build:
 mkdir <mfem-build-dir> ; cd <mfem-build-dir>
