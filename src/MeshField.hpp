05c08264076879dbc9a294b1c58342a2d2d29909
Cameron Smith <smithc11@rpi.edu>
2026-07-14 10:05:02 -0400

    need to match Zienkiewicz, Taylor, Zhu ordering of shape fns
    OmegahTetTest passes

--- src/MeshField.hpp
+++ src/MeshField.hpp
@@ -207,7 +207,7 @@ struct QuadraticTetrahedronToField {
   operator()(MeshField::LO tetNodeIdx, MeshField::LO tetCompIdx,
              MeshField::LO tet, MeshField::Mesh_Topology topo) const {
     assert(topo == MeshField::Tetrahedron);
-    const MeshField::LO tetNode2DofHolder[10] = {0, 1, 2, 3, 3, 4, 5, 0, 2, 1};
+    const MeshField::LO tetNode2DofHolder[10] = {0, 1, 2, 3, 3, 4, 5, 0, 1, 2};
     const MeshField::Mesh_Topology tetNode2DofHolderTopo[10] = {
         MeshField::Vertex, MeshField::Vertex, MeshField::Vertex,
         MeshField::Vertex, MeshField::Edge,   MeshField::Edge,
